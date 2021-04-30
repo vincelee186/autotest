3f713d14ed507352da4b2c4202c41d15a9980745
vincelee186 <vince.lee@pentium.network>
2021-04-30 14:56:06 +0800

    0430

---
+++ test.py
@@ -0,0 +1,45 @@
+from random import randint
+
+# def function(num):
+
+#     result = 1
+
+#     for n in range(1, num+1):
+#         result *= n
+
+#     return result
+
+# m = int(input('m = '))
+# n = int(input('n = '))
+
+# print(function(m) // function(n) // function(m-n))
+
+
+# def roll_dice(n=2):
+    
+#     total = 0
+#     for _ in range(n):
+#         total += randint(1, 6)
+#     return total
+
+# def add(a=0, b=0, c=0):
+#     return a + b + c
+
+# print(roll_dice())
+# print(roll_dice(3))
+# print(add())
+# print(add(1))
+# print(add(1,2))
+# print(add(1,2,3))
+# print(add(c=50, a=100, b=200))
+
+def foo():
+    pass
+def bar():
+    pass
+
+if __name__ == '__main__':
+    print('call foo()')
+    foo()
+    print('call bar()')
+    bar()
